60fbdb76e8733278059b39f3efa20506b2c270c6
MNoichl <noichlmax@hotmail.co.uk>
2025-02-12 15:11:10 +0100

    testing gpu with uvicorn

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
 import base64
 import json
-
 import gradio as gr
 import spaces
 import torch
 from spaces.zero.client import _get_token
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+import uvicorn
+from pathlib import Path
+
+# FastAPI setup
+app = FastAPI()
+static_dir = Path('./static')
+static_dir.mkdir(parents=True, exist_ok=True)
+app.mount("/static", StaticFiles(directory=static_dir), name="static")
 
 @spaces.GPU(duration=4*60) # Not possible with IP-based quotas
 def inner():
@@ -19,4 +28,16 @@ def greet(request: gr.Request, n):
     return json.loads(base64.urlsafe_b64decode(payload).decode())
 
 demo = gr.Interface(fn=greet, inputs=gr.Number(), outputs=gr.JSON())
-demo.launch()
+
+# Mount Gradio app
+app = gr.mount_gradio_app(app, demo, path="/", ssr_mode=False)
+
+app.zerogpu = True
+
+def start_server(app):
+    uvicorn.run(app, host="0.0.0.0", port=7860)
+
+start_server.zerogpu = True
+
+if __name__ == "__main__":
+    start_server(app)
