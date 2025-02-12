6377944648c036d02bcc01d2cb5ae8fb574f407a
MNoichl <noichlmax@hotmail.co.uk>
2025-02-12 18:11:34 +0100

    removed sharing, added ssr & path

--- app.py
+++ app.py
@@ -48,4 +48,4 @@ The Gradio app generates dynamic HTML files and stores them in a static director
 
     new_btn.click(fn=predict, inputs=[text_input], outputs=[markdown, html])
 
-block.launch(debug=True, share=True)
+block.launch(debug=True, share=False, path="/",ssr_mode=False)
