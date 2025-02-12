3c6cb17e45cb6595c40780b2ff0d42f81d44eb42
MNoichl <noichlmax@hotmail.co.uk>
2025-02-12 19:21:06 +0100

    share = True

--- app.py
+++ app.py
@@ -58,4 +58,4 @@ The Gradio app generates dynamic HTML files and stores them in a static director
 
     new_btn.click(fn=predict, inputs=[text_input], outputs=[markdown, html])
 
-block.launch(debug=True, share=False, ssr_mode=False)#,ssr_mode=False
+block.launch(debug=True, share=True, ssr_mode=False)#,ssr_mode=False
